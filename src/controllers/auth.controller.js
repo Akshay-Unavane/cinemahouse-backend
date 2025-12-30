afb4094ceeb2b67f65d3617df5d3f9b89ff137d0
Akshay Unavane <akshayunavane4@gmail.com>
2025-12-30 20:34:21 +0530

    added userId remove id from auth.controller.js

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -29,7 +29,7 @@ export const register = async (req, res) => {
     });
 
     const token = jwt.sign(
-      { id: user._id, username: user.username, email: user.email },
+      { userId: user._id, username: user.username, email: user.email },
       JWT_SECRET,
       { expiresIn: "7d" }
     );
@@ -70,7 +70,7 @@ export const login = async (req, res) => {
     }
 
     const token = jwt.sign(
-      { id: user._id, username: user.username, email: user.email },
+      { userId: user._id, username: user.username, email: user.email },
       JWT_SECRET,
       { expiresIn: "7d" }
     );
@@ -121,7 +121,7 @@ export const resetPassword = async (req, res) => {
 ========================= */
 export const deleteAccount = async (req, res) => {
   try {
-    // const userId = req.user._id; // ✅ comes from JWT
+    const userId = req.user._id; // ✅ comes from JWT
 
     await User.findByIdAndDelete(req.user._id);
 
