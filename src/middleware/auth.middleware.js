82f83dea4c971fc6bae65b215b589d1fea4fa1c2
Akshay Unavane <akshayunavane4@gmail.com>
2025-12-30 20:59:27 +0530

    added JWT_SECRET logic

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,6 +1,8 @@
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
+const JWT_SECRET = process.env.JWT_SECRET || "Your_jwt_secret"
+
 export const authMiddleware = async (req, res, next) => {
   try {
     const authHeader = req.headers.authorization;
@@ -10,7 +12,7 @@ export const authMiddleware = async (req, res, next) => {
     }
 
     const token = authHeader.split(" ")[1];
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, JWT_SECRET);
 
     const user = await User.findById(decoded.userId);
     if (!user) {
@@ -20,6 +22,8 @@ export const authMiddleware = async (req, res, next) => {
     req.user = user;
     next();
   } catch (error) {
+    console.error("AUTH MIDDLEWARE ERROR:", error.message);
+    
     return res.status(401).json({ message: "Invalid token" });
   }
 };
